102204a7b14a5871500089a193c5170c3988f2bc
Comodo21 <vahid.ex3er@gmail.com>
2017-04-13 22:57:47 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+bot_token = "350068970:AAGmCVq2x7fi6dBszVOu6Y_04azXQGtNds"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "6.0"
+sudo_name = "Engineer vahid chavoshi"
+sudo_id = 232504827
+admingp = -1001110269158
+sudo_num = "989031982717"
+sudo_user = "comodo21"
+sudo_ch = "BugSecurity"
